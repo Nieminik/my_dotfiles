82a07be54b501a54168181c11cb4881b91943b41
Nieminik <nieminik@protonmail.com>
2018-05-05 09:22:53 +0200

    Simplify and refactor the code

--- bin/spotify.py
+++ bin/spotify.py
@@ -2,15 +2,12 @@ from subprocess import PIPE, Popen
 import os
 
 def get_metadata(key):
+    command = ['playerctl', 'metadata', key]
     with open(os.devnull, 'w') as devnull:
-        return Popen(['playerctl', 'metadata', key], stdout=PIPE, stderr=devnull).stdout.read()
+        return Popen(command, stdout=PIPE, stderr=devnull).stdout.read()
 
 if __name__ == '__main__':
-    artist = get_metadata('artist').decode()
-    title = get_metadata('title').decode()
-    if all((artist, title)):
-        print('%s - %s' % (artist, title))
-    else:
-        print('')
-
+    for metadata in 'artist', 'title':
+        locals()[metadata] = get_metadata(metadata).decode()
+    print('{} - {}'.format(artist, title) if all((artist, title)) else '')
 
